0fe12813f57c55d5766249e4df3dcab6b6211d8b
Sachin Shivade <shivadesachin244@gmail.com>
2026-08-15 12:30:39 +0530

    Time: 11 ms (82.62%), Space: 21.9 MB (88.03%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = {}
+        
+        for i in range(len(strs)):
+            word = strs[i]
+            sorted_key = "".join(sorted(word))
+            
+            if sorted_key not in anagram_map:
+                anagram_map[sorted_key] = []
+                
+            anagram_map[sorted_key].append(word)
+            
+        return list(anagram_map.values())
